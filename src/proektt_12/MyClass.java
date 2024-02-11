b18a43ded97a55631fd584fa1581c5e4d247c930
EvaIlievska <ilievska_eva@yahoo.com>
2024-02-11 09:26:12 +0100

    vezba23

---
+++ src/proektt_12/MyClass.java
@@ -0,0 +1,12 @@
+package proektt_12;
+public class MyClass {
+
+	public static void main(String[] args) {
+		Vehicle1 v1=new Vehicle1("Red");
+		Vehicle1 v2=new Vehicle1 ();
+		System.out.println(v1);
+		System.out.println(v2);
+		
+	}
+
+}
